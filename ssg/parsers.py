d8ea1de2fd5971dc5f7c39841ae64b7d45f2028b
Raanan Gavrielli <raanan@rhizomatic.biz>
2021-01-26 19:25:38 +0200

    module 2 is done

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -23,3 +23,10 @@ class Parser:
 
     def copy(self, path: Path, source: Path, dest: Path):
         shutil.copy2(path, dest / path.relative_to(source))
+
+
+class ResourceParser(Parser):
+    extensions: List[str] = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(path, source, dest)
